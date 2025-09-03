3ba8e50a816af40b8aa7e219868cd3d2a36c8844
programadorisgod <catapiascastro@gmail.com>
2025-09-03 18:18:36 -0500

    feat add enpoint health

--- main.go
+++ main.go
@@ -16,11 +16,11 @@ func main() {
 
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello world!")
-	})
 	app.Get("/health", func(c *fiber.Ctx) error {
 		return c.SendString("I'm healthy")
+	})
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello world!")
 	})
 
 	app.Post("/login", auth.Login)
