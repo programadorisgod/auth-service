c0a57a712a70ff4f8cc0f64d95159f4501d6a0e3
programadorisgod <catapiascastro@gmail.com>
2025-09-03 22:50:02 -0500

    fix add cors

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	_ "github.com/lib/pq"
 	"github.com/programadorisgod/auth-service/src/config"
 	"github.com/programadorisgod/auth-service/src/controllers/auth"
@@ -15,6 +16,13 @@ func main() {
 	defer config.DB.Close()
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:8080",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:     "Content-Type, Authorization",
+		AllowCredentials: true,
+	}))
 
 	app.Get("/health", func(c *fiber.Ctx) error {
 		return c.SendString("I'm healthy")
